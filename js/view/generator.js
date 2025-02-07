1f9ef821e470d889c8da39087aed7c07cdfab15b
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-02-07 12:47:46 +0200

    Add files via upload

---
+++ js/view/generator.js
@@ -0,0 +1,88 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
+
+
+
+const generatorTemp = () => html`
+
+    <section id="generator">
+              
+        <div class="table">
+         <h1>Select options</h1>
+                <div class="radio-group">
+                    <div>
+                      <input type="radio" id="option1" name="options" value="Simon">
+                      <label for="option1"><img src="" alt="">Simon</label>
+                      <img class="radio-img" src="images/Simon.JPG" alt="">
+                    </div>
+
+                    <div>
+                      <input type="radio" id="option2" name="options" value="Hanna">
+                      <label for="option2">Hanna</label>
+                      <img class="radio-img" src="images/Hanna.JPG" alt="">
+                    </div>
+
+                    <div>
+                       <input type="radio" id="option3" name="options" value="Mathis">
+                       <label for="option3">Mathis</label>
+                       <img class="radio-img" src="images/Mathis.JPG" alt="">
+                    </div>
+
+                    <div>
+                       <input type="radio" id="option4" name="options" value="Ava">
+                       <label for="option4">Ava</label>
+                       <img class="radio-img" src="images/Ava.JPG" alt="">
+                    </div>
+
+                    <!-- <div>
+                        <input type="radio" id="option5" name="options" value="">
+                        <label for="option4">????????</label>
+                        <img class="radio-img" src="images/.JPG" alt="">
+                    </div> -->
+                </div>
+                 <button id="clear-table">Clear Table</button>
+                 <button id="generate-button">Generate</button>
+        </div>
+
+        <div class="table-container">
+            <table id="dynamic-table">
+                <thead>
+                 <tr>
+                     <th>Module</th>
+                     <th>Part number/name</th>
+                     <th>L</th>
+                     <th>B</th>
+                     <th>H</th>
+                 </tr>
+                </thead>
+                <tbody>
+                 <tr class="empty-row">
+                    <td contenteditable="true"></td>
+                    <td contenteditable="true"></td>
+                    <td contenteditable="true"></td>
+                    <td contenteditable="true"></td>
+                    <td contenteditable="true"></td>
+                 </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
+`
+function loadScript() {
+    import("../generatelogic.js")
+    .then((module) => {
+        module.init();
+    })
+    .catch((error) => console.error("Error loading logic script", error));
+} 
+
+let context = null;
+export function showGeneratorView(ctx) {
+    context = ctx;
+    context.hideSlider();
+    context.render(generatorTemp());
+   
+    setTimeout(() => {
+        loadScript();
+    }, 0);
+}
+
--- js/view/home.js
+++ js/view/home.js
@@ -1,1 +1,10 @@
+import { html } from "../../node_modules/lit-html/lit-html.js";
 
+const homeTemp = () => html``
+
+let context = null;
+export function showHomeView(ctx) {
+    context = ctx;
+    context.showSlider()
+    context.render(homeTemp());
+}
