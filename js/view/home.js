366eae60165880f058ce731e417a9d6757bd3f21
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-02-07 20:08:26 +0200

    Update home.js

--- js/view/home.js
+++ js/view/home.js
@@ -1,10 +1,16 @@
 import { html, render } from "https://cdn.jsdelivr.net/npm/lit-html@2.5.0/lit-html.js";
 
-const homeTemp = () => html``
+const homeTemp = () => html``;
 
 let context = null;
 export function showHomeView(ctx) {
     context = ctx;
-    context.showSlider()
+    context.showSlider();
     context.render(homeTemp());
+    
+       window.addEventListener('popstate', function () {
+        document.querySelectorAll('img').forEach(function(img) {
+            img.src = img.src.split('?')[0] + '?' + new Date().getTime();
+        });
+    });
 }
