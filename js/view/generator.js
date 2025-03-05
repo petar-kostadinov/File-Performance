f0a4dce75154b2f749bf69f3a15d63310965d9ab
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-03-05 16:41:40 +0200

    Update generator.js

--- js/view/generator.js
+++ js/view/generator.js
@@ -3,42 +3,58 @@ import { html, render } from "https://cdn.jsdelivr.net/npm/lit-html@2.5.0/lit-ht
 const generatorTemp = () => html`
 
     <section id="generator">
-              
+        <div class="buttons-wrapper">
+           <button id="clear-table">Clear Table</button>
+           <button id="generate-button">Generate</button>
+        </div>
         <div class="table">
-         <h1>Select options</h1>
+            <h1>Select options</h1>
                 <div class="radio-group">
                     <div>
                       <input type="radio" id="option1" name="options" value="Simon">
                       <label for="option1"><img src="" alt="">Simon</label>
-                      <img class="radio-img" src="/File-Performance/images/Simon.jpg" alt="">
+                      <img class="radio-img" src="images/Simon.JPG" alt="">
                     </div>
 
                     <div>
                       <input type="radio" id="option2" name="options" value="Hanna">
                       <label for="option2">Hanna</label>
-                      <img class="radio-img" src="/File-Performance/images/Hanna.jpg" alt="">
+                      <img class="radio-img" src="images/Hanna.JPG" alt="">
                     </div>
 
                     <div>
                        <input type="radio" id="option3" name="options" value="Mathis">
                        <label for="option3">Mathis</label>
-                       <img class="radio-img" src="/File-Performance/images/Mathis.jpg" alt="">
+                       <img class="radio-img" src="images/Mathis.JPG" alt="">
                     </div>
 
                     <div>
                        <input type="radio" id="option4" name="options" value="Ava">
                        <label for="option4">Ava</label>
-                       <img class="radio-img" src="/File-Performance/images/Ava.jpg" alt="">
+                       <img class="radio-img" src="images/Ava.JPG" alt="">
                     </div>
 
                     <!-- <div>
                         <input type="radio" id="option5" name="options" value="">
                         <label for="option4">????????</label>
-                        <img class="radio-img" src="/File-Performance/images/.jpg" alt="">
+                        <img class="radio-img" src="images/.JPG" alt="">
                     </div> -->
+
                 </div>
-                 <button id="clear-table">Clear Table</button>
-                 <button id="generate-button">Generate</button>
+                <div class="radio-group2">
+                        <div>
+                           <input type="radio" id="machine1" name="machines" value="Nesting">
+                           <label for="machine1">Nesting</label>
+                        </div>
+                       
+                        <div>
+                           <input type="radio" id="machine2" name="machines" value="CNC">
+                           <label for="machine2">CNC</label>
+                        </div>
+                </div>
+                 
+                   
+
         </div>
 
         <div class="table-container">
@@ -67,20 +83,19 @@ const generatorTemp = () => html`
 `
 function loadScript() {
     import("../generatelogic.js")
-    .then((module) => {
-        module.init();
-    })
-    .catch((error) => console.error("Error loading logic script", error));
-} 
+        .then((module) => {
+            module.init();
+        })
+        .catch((error) => console.error("Error loading logic script", error));
+}
 
 let context = null;
 export function showGeneratorView(ctx) {
     context = ctx;
     context.hideSlider();
     context.render(generatorTemp());
-   
+
     setTimeout(() => {
         loadScript();
     }, 0);
 }
-
