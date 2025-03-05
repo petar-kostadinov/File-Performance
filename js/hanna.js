7cc408e4f328d9b97641a86003383e9071e50fc5
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-03-05 16:38:52 +0200

    Add files via upload

--- js/hanna.js
+++ js/hanna.js
@@ -1,68 +1,71 @@
-export function modHanna(partName, lValue, bValue, hValue) {
-    let name = partName;
-    let dimX = Number(lValue);
-    let dimY = Number(bValue);
-    let dimZ = Number(hValue)
+export function modHanna(partName, lValue, bValue, hValue, allowance) {
+    const name = partName;
+    const dimX = Number(lValue);
+    const dimY = Number(bValue);
+    const dimZ = Number(hValue);
+    const difference = allowance / 2;
     const cuttingTool = "E005";
     const firstProfileTool = "E033"
     const secondProfileTool = "E021"
     const cutDepth = dimZ + 0.2;
     const profileDepth = 5.5;
     const minArea = 117000;
-    let text = `SetMachiningParameters("AD", 1, 10, 983040, false);
-CreatePolyline("${name}", ${4}, ${dimY - dimY});
-AddSegmentToPolyline(${dimX - 4}, ${dimY - dimY});
-AddArc2PointRadiusToPolyline(${dimX}, 4, 4, false, false);
-AddSegmentToPolyline(${dimX}, ${dimY - 4});
-AddArc2PointRadiusToPolyline(${dimX - 4}, ${dimY}, 4, false, false);
-AddSegmentToPolyline(4, ${dimY});
-AddArc2PointRadiusToPolyline(0, ${dimY - 4}, 4, false, false);
+    let operations = `SetMachiningParameters(${!allowance ? '"AD", 1, 10, 0, false' : '"IJ", 1, 12, 0, false'});
+CreatePolyline("${name}", 4, 0);
+AddSegmentToPolyline(${(dimX + allowance) - 4}, 0);
+AddArc2PointRadiusToPolyline(${dimX + allowance}, 4, 4, false, false);
+AddSegmentToPolyline(${dimX + allowance}, ${(dimY + allowance) - 4});
+AddArc2PointRadiusToPolyline(${(dimX + allowance) - 4}, ${dimY + allowance}, 4, false, false);
+AddSegmentToPolyline(4, ${dimY + allowance});
+AddArc2PointRadiusToPolyline(0, ${(dimY + allowance) - 4}, 4, false, false);
 AddSegmentToPolyline(0, 4);
 AddArc2PointRadiusToPolyline(4, 0, 4, false, false);
 CreateFinishedWorkpieceFromExtrusion("${name}_${dimZ}",${dimZ});
-SetPneumaticHoodPosition(2);
+${allowance ? `CreatePolyline("", ${(difference + 1.172).toFixed(3)}, ${(difference + 1.172).toFixed(3)});
+AddArc2PointRadiusToPolyline(${difference + 4}, ${difference}, 4, false, false);
+AddSegmentToPolyline(${(dimX - 4) + difference}, ${difference});
+AddArc2PointRadiusToPolyline(${dimX + difference}, ${difference + 4}, 4, false, false);
+AddSegmentToPolyline(${dimX + difference}, ${dimY - 4 + difference});
+AddArc2PointRadiusToPolyline(${(dimX - 4) + difference}, ${dimY + difference}, 4, false, false);
+AddSegmentToPolyline(${difference + 4}, ${dimY + difference});
+AddArc2PointRadiusToPolyline(${difference}, ${(dimY - 4) + difference}, 4, false, false);
+AddSegmentToPolyline(${difference}, ${difference + 4});
+AddArc2PointRadiusToPolyline(${(difference + 1.172).toFixed(3)}, ${(difference + 1.172).toFixed(3)}, 4, false, false);` : ""}
+${allowance ? 'SetPneumaticHoodPosition(0);' : 'SetPneumaticHoodPosition(2);'}
 SetToolBlower(0);
 SetApproachStrategy(false, true, 0.5);
-SetRetractStrategy(false, true, 0.5, 10);
-${dimX * dimY <= minArea ? "SetPressureRollers(10);" : "//SetPressureRollers(10);"}
-CreateContour("Perimeter Routing", ${dimX * dimY <= minArea ? cutDepth - 0.7 : cutDepth}, 0, 1, "Workpiece contour", TypeOfProcess.GeneralRouting,
+SetRetractStrategy(false, true, 0.5, 0);
+${dimX * dimY <= minArea && allowance ? "" : "//SetPressureRollers(10);"}
+CreateContour("Perimeter Routing", ${dimX * dimY <= minArea ? (cutDepth - 0.7).toFixed(3) : cutDepth.toFixed(3)}, ${!allowance ? '0' : '1'}, 1, ${!allowance ? '"Workpiece contour"' : '"Contour on geometry"'}, TypeOfProcess.GeneralRouting,
 "${cuttingTool}", "-1", 0, -1, -1, -1, 0);
 ResetPneumaticHood();
 ResetApproachStrategy();
-ResetRetractStrategy();\n\n`;
+ResetRetractStrategy();`
 
     if (dimX > 246 && dimY > 296 && !name.includes("Blind_Part")) {
-        text += `CreatePolyline("", ${dimX / 2}, 70.0000);
-AddSegmentToPolyline(${dimX - 70}, 70.0000);
-AddSegmentToPolyline(${dimX - 70}, ${dimY - 70});
-AddSegmentToPolyline(70, ${dimY - 70});
-AddSegmentToPolyline(70, 70);
-AddSegmentToPolyline(${dimX / 2}, 70);
-SetPneumaticHoodPosition(2);
-ResetApproachStrategy();
-ResetRetractStrategy();
+        operations += `CreatePolyline("", ${(dimX + allowance) / 2}, ${difference + 70});
+AddSegmentToPolyline(${(dimX + difference) - 70}, ${difference + 70});
+AddSegmentToPolyline(${(dimX + difference) - 70}, ${(dimY + difference) - 70});
+AddSegmentToPolyline(${difference + 70}, ${(dimY + difference) - 70});
+AddSegmentToPolyline(${difference + 70}, ${difference + 70});
+AddSegmentToPolyline(${(dimX + allowance) / 2}, ${difference + 70});
+${allowance ? 'SetPneumaticHoodPosition(0);' : 'SetPneumaticHoodPosition(2);'}
+SetToolBlower(0);
 CreateRoughFinish("",${profileDepth},"",TypeOfProcess.GeneralRouting, "${firstProfileTool}", "-1", 0);
-ResetPneumaticHood();
-ResetApproachStrategy();
-ResetRetractStrategy();\n
-CreatePolyline("", -1.0000, 54.85);
-AddSegmentToPolyline(${dimX + 1}, 54.85);
-SetPneumaticHoodPosition(2);
-ResetApproachStrategy();
-ResetRetractStrategy();
+ResetPneumaticHood();\n
+CreatePolyline("", -8.0000, ${allowance ? difference + 54.35 : difference + 54.85});
+AddSegmentToPolyline(${dimX + allowance + 8}, ${allowance ? difference + 54.35 : difference + 54.85});
+${allowance ? 'SetPneumaticHoodPosition(0);' : 'SetPneumaticHoodPosition(2);'}
+SetToolBlower(0);
 CreateRoughFinish("",${profileDepth},"",TypeOfProcess.GeneralRouting, "${secondProfileTool}", "-1", 1);
-ResetPneumaticHood();
-ResetApproachStrategy();
-ResetRetractStrategy();\n
-CreatePolyline("", -1.0000, ${dimY - 54.85});
-AddSegmentToPolyline(${dimX + 1}, ${dimY - 54.85});
-SetPneumaticHoodPosition(2);
- ResetApproachStrategy();
-ResetRetractStrategy();
+ResetPneumaticHood();\n
+CreatePolyline("", -8.0000, ${allowance ? dimY - (54.35 - difference) : dimY - (54.85 - difference)});
+AddSegmentToPolyline(${dimX + allowance + 8}, ${allowance ? dimY - (54.35 - difference) : dimY - (54.85 - difference)});
+${allowance ? 'SetPneumaticHoodPosition(0);' : 'SetPneumaticHoodPosition(2);'}
+SetToolBlower(0);
 CreateRoughFinish("",${profileDepth},"",TypeOfProcess.GeneralRouting, "${secondProfileTool}", "-1", 2);
-ResetPneumaticHood();
-ResetApproachStrategy();
-ResetRetractStrategy();\n\n`
+ResetPneumaticHood();`
     }
+    const text = operations.split("\n").filter(line => line.trim() !== "").join("\n");
     return text;
 }
--- js/mathis797.mjs
+++ js/mathis797.mjs
@@ -1,39 +1,39 @@
-export function mathisB797(lValue, bValue) {
+export function mathisB797(lValue, bValue, difference) {
     const dimX = Number(lValue);
     const dimY = Number(bValue);
     const profileTool = "E031";
     const profileDepth = 7.5;
-    let text = `CreatePolyline("", 107.0991, 701.7725);
-AddArc2PointRadiusToPolyline(122.0974, ${dimY -80}, 15.0000, true, false);
-AddSegmentToPolyline(${dimX -80}, ${dimY -80});
-AddSegmentToPolyline(${dimX -80}, 80);
-AddSegmentToPolyline(122.0974, 80.0000);
-AddArc2PointRadiusToPolyline(107.0991, 95.2275, 15.0000, true, false);
-AddArc2PointRadiusToPolyline(107.3099, 108.8580, 23288.0699, true, false);
-AddArc2PointRadiusToPolyline(107.5286, 122.4883, 23288.0700, true, false);
-AddArc2PointRadiusToPolyline(107.1428, 149.7497, 447.1507, false, false);
-AddArc2PointRadiusToPolyline(104.8309, 179.4034, 448.5381, false, false);
-AddArc2PointRadiusToPolyline(100.3329, 208.8051, 364.2542, false, false);
-AddArc2PointRadiusToPolyline(97.1266, 223.5220, 344.4141, false, false);
-AddArc2PointRadiusToPolyline(93.9153, 238.2378, 349.7798, true, false);
-AddArc2PointRadiusToPolyline(82.5819, 318.0082, 772.2861, true, false);
-AddArc2PointRadiusToPolyline(82.5819, 478.9918, 906.1821, true, false);
-AddArc2PointRadiusToPolyline(93.9153, 558.7622, 772.2861, true, false);
-AddArc2PointRadiusToPolyline(97.1266, 573.4780, 349.7798, true, false);
-AddArc2PointRadiusToPolyline(100.3329, 588.1949, 344.4141, false, false);
-AddArc2PointRadiusToPolyline(104.8309, 617.5966, 364.2542, false, false);
-AddArc2PointRadiusToPolyline(107.1428, 647.2503, 448.5381, false, false);
-AddArc2PointRadiusToPolyline(107.5286, 674.5117, 447.1507, false, false);
-AddArc2PointRadiusToPolyline(107.3099, 688.1420, 23288.0697, true, false);
-AddArc2PointRadiusToPolyline(107.0991, 701.7725, 23288.0704, true, false);
-SetPneumaticHoodPosition(2);
+    let operations = `CreatePolyline("", ${difference + 107.0991}, ${difference + 701.7725});
+AddArc2PointRadiusToPolyline(${difference + 122.0974}, ${(difference * 2 + dimY) - (difference + 80)}, 15.0000, true, false);
+AddSegmentToPolyline(${(difference * 2 + dimX) - (difference + 80)}, ${(difference * 2 + dimY) - (difference + 80)});
+AddSegmentToPolyline(${(difference * 2 + dimX) - (difference + 80)}, ${difference + 80});
+AddSegmentToPolyline(${difference + 122.0974}, ${difference + 80.0000});
+AddArc2PointRadiusToPolyline(${difference + 107.0991}, ${difference + 95.2275}, 15.0000, true, false);
+AddArc2PointRadiusToPolyline(${difference + 107.3099}, ${difference + 108.8580}, 23288.0699, true, false);
+AddArc2PointRadiusToPolyline(${difference + 107.5286}, ${difference + 122.4883}, 23288.0700, true, false);
+AddArc2PointRadiusToPolyline(${difference + 107.1428}, ${difference + 149.7497}, 447.1507, false, false);
+AddArc2PointRadiusToPolyline(${difference + 104.8309}, ${difference + 179.4034}, 448.5381, false, false);
+AddArc2PointRadiusToPolyline(${difference + 100.3329}, ${difference + 208.8051}, 364.2542, false, false);
+AddArc2PointRadiusToPolyline(${difference + 97.1266}, ${difference + 223.5220}, 344.4141, false, false);
+AddArc2PointRadiusToPolyline(${difference + 93.9153}, ${difference + 238.2378}, 349.7798, true, false);
+AddArc2PointRadiusToPolyline(${difference + 82.5819}, ${difference + 318.0082}, 772.2861, true, false);
+AddArc2PointRadiusToPolyline(${difference + 82.5819}, ${difference + 478.9918}, 906.1821, true, false);
+AddArc2PointRadiusToPolyline(${difference + 93.9153}, ${difference + 558.7622}, 772.2861, true, false);
+AddArc2PointRadiusToPolyline(${difference + 97.1266}, ${difference + 573.4780}, 349.7798, true, false);
+AddArc2PointRadiusToPolyline(${difference + 100.3329}, ${difference + 588.1949}, 344.4141, false, false);
+AddArc2PointRadiusToPolyline(${difference + 104.8309}, ${difference + 617.5966}, 364.2542, false, false);
+AddArc2PointRadiusToPolyline(${difference + 107.1428}, ${difference + 647.2503}, 448.5381, false, false);
+AddArc2PointRadiusToPolyline(${difference + 107.5286}, ${difference + 674.5117}, 447.1507, false, false);
+AddArc2PointRadiusToPolyline(${difference + 107.3099}, ${difference + 688.1420}, 23288.0697, true, false);
+AddArc2PointRadiusToPolyline(${difference + 107.0991}, ${difference + 701.7725}, 23288.0704, true, false);
+${difference ? 'SetPneumaticHoodPosition(0);' : 'SetPneumaticHoodPosition(2);'}
 SetToolBlower(0);
-ResetApproachStrategy();
-ResetRetractStrategy();
 SetCompensationMode(false);
 CreateRoughFinish("",${profileDepth},"",TypeOfProcess.GeneralRouting, "${profileTool}", "-1", 0);
 ResetPneumaticHood();
 ResetApproachStrategy();
 ResetRetractStrategy();`
+
+    const text = operations.split("\n").filter(line => line.trim() !== "").join("\n");
     return text;
 }
