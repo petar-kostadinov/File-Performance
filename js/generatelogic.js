0d103c2a058468c1ff67039f63a58a7dd56843a9
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-03-10 09:14:38 +0200

    Add files via upload

--- js/generatelogic.js
+++ js/generatelogic.js
@@ -1,4 +1,3 @@
-
 import { modAva } from "./ava.js";
 import { modHanna } from "./hanna.js";
 import { modMathis } from "./mathis.js";
@@ -7,13 +6,19 @@ import { modSimon } from "./simon.js";
 var tableRows;
 var currentUrl = window.location.href;
 let allowance;
+var tableBody = document.querySelector('tbody');
+var modal = document.getElementById('modal');
+var modalTitle = document.getElementById('modal-title');
+var modalMessage = document.getElementById('modal-message');
+var okButton = document.getElementById('ok-button');
+var zip = new JSZip();
 
 export function init() {
-    
+
     var cells = document.querySelectorAll('tbody td[contenteditable="true"]');
     cells.forEach(function (cell) {
         cell.addEventListener('blur', handleCellBlur);
-        if (cell.cellIndex === 2 || cell.cellIndex === 3 || cell.cellIndex === 4) {
+        if (cell.cellIndex === 0 || cell.cellIndex === 1 || cell.cellIndex === 2 || cell.cellIndex === 3 || cell.cellIndex === 4) {
             cell.addEventListener('blur', validateColumn);
         }
     });
@@ -29,46 +34,46 @@ export function init() {
 
 function validateColumn(event) {
     var inputValue = event.target.textContent.trim();
-    var modal = document.getElementById('modal');
-    var modalTitle = document.getElementById('modal-title');
-    var modalMessage = document.getElementById('modal-message');
-    var okButton = document.getElementById('ok-button');
+    var cellIndex = Array.from(event.target.parentElement.children).indexOf(event.target);
 
-    okButton.onclick = function() {
+    okButton.onclick = function () {
         modal.style.display = 'none';
     }
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
+    if (cellIndex === 0 || cellIndex === 1) {
+        if (inputValue === '') {
+            event.target.classList.add('invalid-input');
+            modalTitle.textContent = currentUrl;
+            modalMessage.textContent = `Моля попълнете полето`;
+            modal.style.display = 'block';
+        } else {
+            event.target.classList.remove('invalid-input');
         }
-    }
-    
-    if (inputValue.includes(',')) {
-        event.target.classList.add('invalid-input');
-        modalTitle.textContent = currentUrl;
-        modalMessage.textContent = `Моля заменете запетаята с точка`;
-        modal.style.display = 'block';
-    }
-
-    else if (isNaN(parseFloat(inputValue))) {
-        event.target.classList.add('invalid-input');
-        modalTitle.textContent = currentUrl;
-        modalMessage.textContent = 'Невалидна стойност за клетка. Моля, въведете число.';
-        modal.style.display = 'block';
     }
 
     else {
-        event.target.classList.remove('invalid-input');
-        //messageElement.style.display = 'none';
+
+        if (inputValue.includes(',')) {
+            event.target.classList.add('invalid-input');
+            modalTitle.textContent = currentUrl;
+            modalMessage.textContent = `Моля заменете запетаята с точка`;
+            modal.style.display = 'block';
+        } else if (isNaN(parseFloat(inputValue))) {
+            event.target.classList.add('invalid-input');
+            modalTitle.textContent = currentUrl;
+            modalMessage.textContent = 'Невалидна стойност за клетка. Моля, въведете число.';
+            modal.style.display = 'block';
+        } else {
+            event.target.classList.remove('invalid-input');
+        }
     }
 }
-
-
 
 function handleCellBlur(event) {
     var cell = event.target;
     var row = cell.parentElement;
+    var allRows = tableBody.querySelectorAll('tr');
+    var lastRow = allRows[allRows.length - 1];
     var cells = row.querySelectorAll('td[contenteditable="true"]');
     var lastCell = cells[cells.length - 1];
     var isLastCell = cell === lastCell;
@@ -76,7 +81,7 @@ function handleCellBlur(event) {
         return cell.textContent.trim() !== '';
     });
 
-    if (isLastCell && isRowFilled) {
+    if (isLastCell && isRowFilled && row === lastRow) {
         addEmptyRow();
         updateTableRows();
     }
@@ -85,14 +90,13 @@ function handleCellBlur(event) {
 function addEmptyRow() {
     var emptyRow = document.createElement('tr');
     emptyRow.classList.add('empty-row');
-    var tableBody = document.querySelector('tbody');
     var cellCount = document.querySelector('thead tr').childElementCount;
     for (var i = 0; i < cellCount; i++) {
         var cell = document.createElement('td');
         cell.setAttribute('contenteditable', 'true');
         emptyRow.appendChild(cell);
         cell.addEventListener('blur', handleCellBlur);
-        if (i === 2 || i === 3 || i === 4) {
+        if (i === 0 || i === 1 || i === 2 || i === 3 || i === 4) {
             cell.addEventListener('blur', validateColumn);
         }
     }
@@ -105,21 +109,12 @@ function updateTableRows() {
 
 function generateLogic() {
 
-    var modal = document.getElementById('modal');
-    var modalTitle = document.getElementById('modal-title');
-    var modalMessage = document.getElementById('modal-message');
-    var okButton = document.getElementById('ok-button');
+    let message;
 
-    okButton.onclick = function() {
+    okButton.onclick = function () {
         modal.style.display = 'none';
     }
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
-    
     if (!tableRows || tableRows.length === 0) {
         modalTitle.textContent = currentUrl;
         modalMessage.textContent = 'Таблицата е празна.';
@@ -129,7 +124,6 @@ function generateLogic() {
 
     var selectedOption = document.querySelector('input[name="options"]:checked');
     var selectedMachine = document.querySelector('input[name="machines"]:checked');
-
 
     if (!selectedOption) {
         modalTitle.textContent = currentUrl;
@@ -144,8 +138,8 @@ function generateLogic() {
         modal.style.display = 'block';
         return;
     }
-    var optionValue = selectedOption.value; 
-    var machineValue = selectedMachine.value; 
+    var optionValue = selectedOption.value; // Стойността на опцията
+    var machineValue = selectedMachine.value; // На коя машина
 
     if (machineValue === 'CNC') {
         allowance = 4;
@@ -153,15 +147,12 @@ function generateLogic() {
         allowance = 0;
     }
 
-
-    var zip = new JSZip();
-
     tableRows.forEach(function (row) {
-        var moduleName = row.children[0].textContent.trim(); 
-        var partName = row.children[1].textContent.trim(); 
-        var lValue = row.children[2].textContent.trim(); 
-        var bValue = row.children[3].textContent.trim(); 
-        var hValue = row.children[4].textContent.trim(); 
+        var moduleName = row.children[0].textContent.trim(); // Името на модула от колона 1
+        var partName = row.children[1].textContent.trim(); // Името на частта от колона 2
+        var lValue = row.children[2].textContent.trim(); // Стойността на L от колона 3
+        var bValue = row.children[3].textContent.trim(); // Стойността на B от колона 4
+        var hValue = row.children[4].textContent.trim(); // Стойността на H от колона 5
 
         if (moduleName === '' && partName === '' && lValue === '' && bValue === '' && hValue === '') {
             return;
@@ -176,7 +167,9 @@ function generateLogic() {
                 fileContent = modHanna(partName, lValue, bValue, hValue, allowance);
                 break;
             case 'Mathis':
-                fileContent = modMathis(moduleName, partName, lValue, bValue, hValue, allowance);
+                const result = modMathis(moduleName, partName, lValue, bValue, hValue, allowance);
+                fileContent = result.text ? result.text : result;
+                message = result.message;
                 break;
             case 'Ava':
                 fileContent = modAva(partName, lValue, bValue, hValue, allowance);
@@ -191,18 +184,28 @@ function generateLogic() {
         zip.folder(folderName).file(fileName, fileContent);
     });
 
-    zip.generateAsync({ type: 'blob' }).then(function (content) {
-        saveAs(content, `${optionValue}.zip`);
-    });
+    creataZip(zip, optionValue, message);
 
-    modalTitle.textContent = currentUrl;
-    modalMessage.textContent = 'Файловете са генерирани и архивирани в ZIP формат.';
-    modal.style.display = 'block';
 }
 
+function creataZip(zip, optionValue, message) {
+    modalTitle.textContent = currentUrl;
+    modalMessage.innerHTML = !message
+        ? 'Файловете ще бъдат генерирани и архивирани в ZIP формат.'
+        : `<strong>${message}</strong><br>Файловете ще бъдат генерирани и архивирани в ZIP формат.`;
+
+    modal.style.display = 'block';
+
+    okButton.onclick = function () {
+        modal.style.display = 'none';
+
+        zip.generateAsync({ type: 'blob' }).then(function (content) {
+            saveAs(content, `${optionValue}.zip`);
+        });
+    };
+}
 
 function clearTable() {
-    var tableBody = document.querySelector('tbody');
     tableBody.innerHTML = '';
     updateTableRows();
     addEmptyRow();
